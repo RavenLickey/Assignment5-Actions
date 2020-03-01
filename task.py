a9a40a6262e6479b2a2d0072d22331c508d49933
RavenLickey <ravenlickey@gmail.com>
2020-02-29 17:03:18 -0800

    circleArea added

--- task.py
+++ task.py
@@ -1,2 +1,9 @@
+import math
+
+
 def firstrun():
     return "success"
+
+
+def circleArea(radius):
+    return math.pi*radius*radius
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 import unittest
 import task
+import math
 
 
 class TestCase(unittest.TestCase):
@@ -11,6 +12,14 @@ class TestCase(unittest.TestCase):
     def test2(self):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
+
+    def test_circleArea(self):
+        input1 = 1
+        output1 = math.pi
+        self.assertEqual(output1, task.circleArea(input1))
+        input2 = 0
+        output2 = 0
+        self.assertEqual(output2, task.circleArea(input2))
 
 
 if __name__ == '__main__':
